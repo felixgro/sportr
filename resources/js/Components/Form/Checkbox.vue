500ec30f41b039094547f7f2ff13f679c10a70ec
Felix Grohs <contact@felixgrohs.at>
2021-01-17 22:15:59 +0100

    update form components

---
+++ resources/js/Components/Form/Checkbox.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+	<label :for="name" class="flex items-center">
+		<input class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            type="checkbox"
+            :value="value"
+            v-model="proxyChecked"
+            :id="name"
+		>
+
+		<span class="font-medium text-sm text-gray-600 ml-2">
+			<slot />
+		</span>
+	</label>
+    <span v-if="hasError" class="text-sm text-red-600">
+		{{ getError }}
+	</span>
+</div>
+</template>
+
+<script>
+import FormField from '@/Mixins/FormField'
+
+export default {
+    mixins: [FormField],
+    model: {
+        prop: "checked",
+        event: "change",
+    },
+    props: {
+        checked: {
+            type: [Array, Boolean],
+            default: false,
+        },
+        value: {
+            default: null,
+        },
+    },
+    computed: {
+        proxyChecked: {
+            get() {
+                return this.checked;
+            },
+            set(val) {
+                this.$emit("change", val);
+            },
+        }
+    }
+}
+</script>
+
---
+++ resources/js/Components/Form/TextInput.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<label :for="name" class="font-medium text-sm text-gray-600">{{ label }}</label>
+
+    	<input class="block w-full mt-1 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+			:type="type"
+			:value="value"
+			:id="name"
+			@input="$emit('input', $event.target.value)
+		">
+
+		 <span v-if="hasError" class="text-sm text-red-600">
+			 {{ getError }}
+		 </span>
+	</div>
+</template>
+
+<script>
+import FormField from '@/Mixins/FormField'
+
+export default {
+	mixins: [FormField],
+	props: {
+		value: {
+			type: String,
+			required: true
+		},
+		label: {
+			type: String,
+			required: true
+		},
+		type: {
+			type: String,
+			default: 'text'
+		}
+	}
+}
+</script>
