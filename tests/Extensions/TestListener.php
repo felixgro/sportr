0f922797aeb5a05a98a53a9de6433911ff481d29
Felix Grohs <contact@felixgrohs.at>
2021-01-19 20:26:15 +0100

    refactor TestListener

--- tests/Extensions/TestListener.php
+++ tests/Extensions/TestListener.php
@@ -7,12 +7,20 @@ use PHPUnit\Runner\{BeforeFirstTestHook, AfterLastTestHook};
 
 final class TestListener implements BeforeFirstTestHook, AfterLastTestHook
 {
+	/**
+	 * Migrate & seed testing database before running.
+	 *
+	 */
 	public function executeBeforeFirstTest(): void
 	{
 		shell_exec('php artisan migrate --env=testing');
 		shell_exec('php artisan db:seed --env=testing');
 	}
 
+	/**
+	 * Reset testing database when done.
+	 *
+	 */
 	public function executeAfterLastTest(): void
 	{
 		shell_exec('php artisan migrate:reset');
