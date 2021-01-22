9b7269f670df6a109c1bcebca9766afae80b8076
Felix Grohs <contact@felixgrohs.at>
2021-01-22 13:47:44 +0100

    add Event Model

---
+++ app/Models/Event.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'title',
+        'date'
+    ];
+
+    protected $casts = [
+        'date' => 'datetime',
+    ];
+
+    /**
+     * Get sport of event.
+     *
+     * @return App\Models\Sport
+     */
+    public function sport()
+    {
+        return $this->belongsTo(Sport::class);
+    }
+
+    /**
+     * Get location of event.
+     *
+     * @return App\Models\Location
+     */
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    /**
+     * Get participating teams.
+     *
+     * @return Illuminate\Support\Collection
+     */
+    public function teams()
+    {
+        return $this->belongsToMany(Team::class);
+    }
+}
---
+++ database/migrations/2021_01_22_113513_create_events_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+
+            $table->foreignId('sport_id')->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->foreignId('location_id')->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->timestamp('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
