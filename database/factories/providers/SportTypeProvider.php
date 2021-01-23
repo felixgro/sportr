2c61dbff0971f1739a5568c3c5fe2cdb3b946a4e
Felix Grohs <contact@felixgrohs.at>
2021-01-23 18:27:03 +0100

    add faker provider for sport

---
+++ database/factories/providers/SportTypeProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories\Providers;
+
+use Faker\Provider\Base;
+
+class SportTypeProvider extends Base
+{
+	/**
+	 * Default sports from config/sportr.php
+	 *
+	 * @return Illuminate\Support\Collection
+	 */
+	protected function availableSportTypes()
+	{
+		return collect(config('sportr.default_sports'));
+	}
+
+	/**
+	 * Returns a random sport.
+	 *
+	 * @return string
+	 */
+	public function sport()
+	{
+		return $this->availableSportTypes()
+			->pluck('title')
+			->random();
+	}
+}
