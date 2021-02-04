1d9d4fb2add8fdd05b31a65565b8bda4cd640845
Felix Grohs <contact@felixgrohs.at>
2021-02-04 20:29:06 +0100

    add unique key to team name

---
+++ database/factories/Providers/TeamNameProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories\Providers;
+
+use Faker\Provider\Base;
+
+class TeamNameProvider extends Base
+{
+	/**
+	 * Creatures for team name generation.
+	 *
+	 * @return Illuminate\Support\Collection
+	 */
+	protected function creatures()
+	{
+		return [
+			'ants', 'bats', 'bears', 'bees', 'birds', 'buffalo', 'cats', 'chickens',
+			'cattle', 'dogs', 'dolphins', 'ducks', 'elephants', 'fishes', 'foxes',
+			'frogs', 'geese', 'goats', 'horses', 'kangaroos', 'lions', 'monkeys',
+			'owls', 'oxen', 'penguins', 'people', 'pigs', 'rabbits', 'sheep', 'tigers',
+			'whales', 'wolves', 'zebras', 'banshees', 'crows', 'black cats', 'chimeras',
+			'ghosts', 'conspirators', 'dragons', 'dwarfs', 'elves', 'enchanters', 'exorcists',
+			'sons', 'foes', 'giants', 'gnomes', 'goblins', 'gooses', 'griffins', 'lycanthropes',
+			'nemesis', 'ogres', 'oracles', 'prophets', 'sorcerers', 'spiders', 'spirits', 'vampires',
+			'warlocks', 'vixens', 'werewolves', 'witches', 'worshipers', 'zombies', 'druids'
+		];
+	}
+
+	/**
+	 * Returns a random sport from config.
+	 *
+	 * @return string
+	 */
+	public function team(): string
+	{
+		return ucwords($this->generator->state . ' ' . static::randomElement($this->creatures()));
+	}
+}
--- database/factories/TeamFactory.php
+++ database/factories/TeamFactory.php
@@ -2,8 +2,8 @@
 
 namespace Database\Factories;
 
-use App\Models\Sport;
-use App\Models\Team;
+use App\Models\{Sport, Team};
+use Database\Factories\Providers\TeamNameProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TeamFactory extends Factory
@@ -16,6 +16,18 @@ class TeamFactory extends Factory
     protected $model = Team::class;
 
     /**
+     * Add custom faker provider for team names.
+     *
+     * @return void
+     */
+    public function __construct(...$args)
+    {
+        parent::__construct(...$args);
+
+        $this->faker->addProvider(new TeamNameProvider($this->faker));
+    }
+
+    /**
      * Define the model's default state.
      *
      * @return array
@@ -23,7 +35,7 @@ class TeamFactory extends Factory
     public function definition()
     {
         return [
-            'title' => $this->faker->word,
+            'title' => $this->faker->unique()->team,
             'sport_id' => Sport::all()->random()->id
         ];
     }
