afb5eded1be79c30f12e7ba0c45947d44cd8665b
Felix Grohs <contact@felixgrohs.at>
2021-02-06 17:30:14 +0100

    add event title provider to EventFactory

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\Event;
 use App\Models\Location;
 use App\Models\Sport;
+use Database\Factories\Providers\EventNameProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class EventFactory extends Factory
@@ -17,15 +18,29 @@ class EventFactory extends Factory
     protected $model = Event::class;
 
     /**
+     * Add custom faker provider for event titles.
+     *
+     * @return void
+     */
+    public function __construct(...$args)
+    {
+        parent::__construct(...$args);
+
+        $this->faker->addProvider(new EventNameProvider($this->faker));
+    }
+
+    /**
      * Define the model's default state.
      *
      * @return array
      */
     public function definition()
     {
+        $sport = Sport::all()->random();
+
         return [
-            'title' => $this->faker->word,
-            'sport_id' => Sport::all()->random()->id,
+            'title' => "{$this->faker->eventPlace} {$sport->title} {$this->faker->eventNoun}",
+            'sport_id' => $sport->id,
             'location_id' => Location::factory(),
             'date' => $this->faker->dateTimeBetween('now', '+2 years')
         ];
---
+++ database/factories/Providers/EventNameProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories\Providers;
+
+use Faker\Provider\Base;
+
+class EventNameProvider extends Base
+{
+	protected $places = [
+		'world', 'european', 'american', 'asian'
+	];
+
+	protected $nouns = [
+		'cup', 'championship', 'tournament', 'battle', 'finals', 'olympics',
+	];
+
+	/**
+	 * Returns a random event place.
+	 *
+	 * @return string
+	 */
+	public function eventPlace()
+	{
+		return ucwords(static::randomElement($this->places));
+	}
+
+	/**
+	 * Returns a random event noun.
+	 *
+	 * @return string
+	 */
+	public function eventNoun()
+	{
+		return ucWords(static::randomElement($this->nouns));
+	}
+}
