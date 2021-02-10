1f79afc38df7a25c424d3b2be438479e57425b37
Felix Grohs <contact@felixgrohs.at>
2021-02-10 19:20:00 +0100

    refactor event seeder

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -22,15 +22,17 @@ class EventSeeder extends Seeder
         $amount = $amount <= 50 ? $amount : 50;
 
         while ($amount > 0) {
-            $sport = Sport::inRandomOrder()->first();
-
             // 50:50
             $hasScored = rand(0, 2) % 2 == 0;
 
-            Event::factory()->for($sport)->hasAttached(
-                Team::factory()->count(2)->for($sport),
-                ['score' => $hasScored ? rand(0, 5) : null]
-            )->create();
+            $event = Event::factory()->create();
+
+            $teams = Team::factory()->for($event->sport)->count(2)->create();
+
+            $event->teams()->attach([
+                $teams[0]->id => ['score' => $hasScored ? rand(0, 5) : null],
+                $teams[1]->id => ['score' => $hasScored ? rand(0, 5) : null],
+            ]);
 
             $amount--;
         }
