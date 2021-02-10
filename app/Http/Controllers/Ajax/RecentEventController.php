4cd2199070fd641dfc52bb156c920e30274f8895
Felix Grohs <contact@felixgrohs.at>
2021-02-10 19:17:51 +0100

    add ajax controllers

---
+++ app/Http/Controllers/Ajax/RecentEventController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Ajax;
+
+use App\Http\Controllers\Controller;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class RecentEventController extends Controller
+{
+	public function __invoke(Request $request)
+	{
+		return Event::scored()
+			->orderBy('date', 'desc')
+			->with(['sport', 'teams', 'location'])
+			->paginate(4);
+	}
+}
---
+++ app/Http/Controllers/Ajax/UpcommingEventController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Ajax;
+
+use App\Http\Controllers\Controller;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class UpcommingEventController extends Controller
+{
+	public function __invoke(Request $request)
+	{
+		return Event::upcomming()
+			->orderBy('date', 'asc')
+			->with(['sport', 'teams', 'location'])
+			->paginate(4);
+	}
+}
