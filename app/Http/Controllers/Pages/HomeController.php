dd0106d8dd4719d67de82fac7b10b7207b16c5b1
Felix Grohs <contact@felixgrohs.at>
2021-02-10 19:18:47 +0100

    append route to sports in HomeController

--- app/Http/Controllers/Pages/HomeController.php
+++ app/Http/Controllers/Pages/HomeController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Pages;
 
 use App\Http\Controllers\Controller;
+use App\Models\Sport;
 use Inertia\Inertia;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        return Inertia::render('Home');
+        return Inertia::render('Home/Show', [
+            'sports' => fn () => Sport::all()->append('route')
+        ]);
     }
 }
