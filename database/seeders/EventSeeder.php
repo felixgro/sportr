5f84e9e375df182b08b16ed247453befa755b148
Felix Grohs <contact@felixgrohs.at>
2021-02-12 11:41:56 +0100

    improved event seeder

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -3,9 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Event;
-use App\Models\Sport;
 use App\Models\Team;
 use Illuminate\Database\Seeder;
+use Faker\Factory;
 
 class EventSeeder extends Seeder
 {
@@ -18,17 +18,20 @@ class EventSeeder extends Seeder
      */
     public function run(int $amount = 10)
     {
+        $faker = Factory::create();
+
         // Limit max Amount to 50 to prevent duplicate entry for unique title key of teams
         $amount = $amount <= 50 ? $amount : 50;
 
         while ($amount > 0) {
-            // 50:50
+            // 50:50 Chance
             $hasScored = rand(0, 2) % 2 == 0;
+            $date = $hasScored ? $faker->dateTimeBetween('-2 years', 'now') : $faker->dateTimeBetween('now', '+2 years');
 
-            $event = Event::factory()->create();
+            $event = Event::factory()->create(['date' => $date]);
 
+            // Assign 2 new teams
             $teams = Team::factory()->for($event->sport)->count(2)->create();
-
             $event->teams()->attach([
                 $teams[0]->id => ['score' => $hasScored ? rand(0, 5) : null],
                 $teams[1]->id => ['score' => $hasScored ? rand(0, 5) : null],
