55c2639c709d25c5ebcd33ee8c935de5827a0159
Felix Grohs <contact@felixgrohs.at>
2021-02-15 19:15:38 +0100

    add custom register response class

---
+++ app/Http/Responses/RegisterResponse.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Laravel\Fortify\Contracts\RegisterResponse as RegisterResponseContract;
+
+
+class RegisterResponse implements RegisterResponseContract
+{
+
+	/**
+	 * Redirect fresh registered user to /favsports.
+	 * -> vue component for favorite sports choice.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 */
+	public function toResponse($request)
+	{
+		return redirect()->route('favsports.index')
+			->with('freshlyRegistered', true);
+	}
+}
