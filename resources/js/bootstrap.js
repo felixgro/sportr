d0dd8835dc5ed51513aa2a338611c197ebebbcff
Felix Grohs <contact@felixgrohs.at>
2021-02-15 19:17:34 +0100

    implement fav sport feature for client

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,3 +9,11 @@ window._ = require('lodash');
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Use ray app to debug.
+ *
+ */
+const { ray } = require('js-ray');
+
+window.ray = ray;
