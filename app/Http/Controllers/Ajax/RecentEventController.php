f3c74f53ce413984ce485031e322d679971d4cf0
Felix Grohs <contact@felixgrohs.at>
2021-02-15 19:16:38 +0100

    include sport pref in recent and upcomming events

--- app/Http/Controllers/Ajax/RecentEventController.php
+++ app/Http/Controllers/Ajax/RecentEventController.php
@@ -4,14 +4,26 @@ namespace App\Http\Controllers\Ajax;
 
 use App\Http\Controllers\Controller;
 use App\Models\Event;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RecentEventController extends Controller
 {
-    public function __invoke(Request $request)
+
+    /**
+     * Get recently scored events. If an authenticated user
+     * exists filter events based on favorite sports.
+     *
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function __invoke()
     {
-        return Event::scored()
-            ->orderBy('date', 'desc')
+        $events = Event::query()->scored();
+
+        if (Auth::user()) {
+            $events->whereIn('sport_id', Auth::user()->favSports->pluck('id'));
+        }
+
+        return $events->orderBy('date', 'desc')
             ->with(['sport', 'teams', 'location'])
             ->paginate(4);
     }
--- app/Http/Controllers/Ajax/UpcommingEventController.php
+++ app/Http/Controllers/Ajax/UpcommingEventController.php
@@ -4,14 +4,26 @@ namespace App\Http\Controllers\Ajax;
 
 use App\Http\Controllers\Controller;
 use App\Models\Event;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UpcommingEventController extends Controller
 {
-    public function __invoke(Request $request)
+
+    /**
+     * Get upcomming future events. If an authenticated user
+     * exists filter events based on favorite sports.
+     *
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function __invoke()
     {
-        return Event::upcomming()
-            ->orderBy('date', 'asc')
+        $events = Event::query()->upcomming();
+
+        if (Auth::user()) {
+            $events->whereIn('sport_id', Auth::user()->favSports->pluck('id'));
+        }
+
+        return $events->orderBy('date', 'asc')
             ->with(['sport', 'teams', 'location'])
             ->paginate(4);
     }
