78e1cc750234bd07cc8340881078b83feecf7eaf
Felix Grohs <felixgro@users.noreply.github.com>
2021-02-19 10:36:54 +0000

    Apply fixes from StyleCI

--- app/Services/ReportService.php
+++ app/Services/ReportService.php
@@ -2,69 +2,70 @@
 
 namespace App\Services;
 
-use App\Models\{Event, User};
+use App\Models\Event;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class ReportService
 {
-	/**
-	 * Related event.
-	 *
-	 * @var \App\Models\Event
-	 */
-	protected $event;
+    /**
+     * Related event.
+     *
+     * @var \App\Models\Event
+     */
+    protected $event;
 
-	/**
-	 * Related user as author
-	 *
-	 * @var \App\Models\User
-	 */
-	protected $author;
+    /**
+     * Related user as author.
+     *
+     * @var \App\Models\User
+     */
+    protected $author;
 
-	/**
-	 * Writes and stores a new report in the database.
-	 *
-	 * @param  \App\Models\Event $event
-	 * @param  \App\Models\User $author
-	 * @param  array $attr
-	 * @return \App\Models\Report
-	 */
-	public function store(array $attr)
-	{
-		if (!$this->author && Auth::check()) {
-			$this->author = Auth::user();
-		}
+    /**
+     * Writes and stores a new report in the database.
+     *
+     * @param  \App\Models\Event $event
+     * @param  \App\Models\User $author
+     * @param  array $attr
+     * @return \App\Models\Report
+     */
+    public function store(array $attr)
+    {
+        if (! $this->author && Auth::check()) {
+            $this->author = Auth::user();
+        }
 
-		return $this->event->reports()->create([
-			'subject' => $attr['subject'],
-			'description' => $attr['description'],
-			'user_id' => $this->author->id
-		]);
-	}
+        return $this->event->reports()->create([
+            'subject' => $attr['subject'],
+            'description' => $attr['description'],
+            'user_id' => $this->author->id
+        ]);
+    }
 
-	/**
-	 * Defines related event.
-	 *
-	 * @param \App\Models\Event $event
-	 * @return ReportService
-	 */
-	public function forEvent(Event $event)
-	{
-		$this->event = $event;
+    /**
+     * Defines related event.
+     *
+     * @param \App\Models\Event $event
+     * @return ReportService
+     */
+    public function forEvent(Event $event)
+    {
+        $this->event = $event;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Defines related author.
-	 *
-	 * @param \App\Models\User $user
-	 * @return ReportService
-	 */
-	public function withAuthor(User $author)
-	{
-		$this->author = $author;
+    /**
+     * Defines related author.
+     *
+     * @param \App\Models\User $user
+     * @return ReportService
+     */
+    public function withAuthor(User $author)
+    {
+        $this->author = $author;
 
-		return $this;
-	}
+        return $this;
+    }
 }
