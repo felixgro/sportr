0604754b7c75e59b5582d300a20051421b1dd9f9
Felix Grohs <me@felixgrohs.at>
2021-03-07 21:06:31 +0100

    refactoring

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,47 +2,47 @@
 
 namespace App\Console;
 
-use App\Console\Commands\CreateDatabaseCommand;
-use App\Console\Commands\ResetCommand;
-use App\Console\Commands\SeedCommand;
-use App\Console\Commands\SetupCommand;
+use App\Console\Commands\MakeDatabaseCommand;
+use App\Console\Commands\SetupDockerCommand;
+use App\Console\Commands\SetupLocalCommand;
+use App\Console\Commands\SetupUndoCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        SetupCommand::class,
-        ResetCommand::class,
-        SeedCommand::class,
-        CreateDatabaseCommand::class
-    ];
+	/**
+	 * The Artisan commands provided by your application.
+	 *
+	 * @var array
+	 */
+	protected $commands = [
+		MakeDatabaseCommand::class,
+		SetupLocalCommand::class,
+		SetupDockerCommand::class,
+		SetupUndoCommand::class,
+	];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')->hourly();
-    }
+	/**
+	 * Define the application's command schedule.
+	 *
+	 * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+	 * @return void
+	 */
+	protected function schedule(Schedule $schedule)
+	{
+		// $schedule->command('inspire')->hourly();
+	}
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        $this->load(__DIR__ . '/Commands');
+	/**
+	 * Register the commands for the application.
+	 *
+	 * @return void
+	 */
+	protected function commands()
+	{
+		$this->load(__DIR__ . '/Commands');
 
-        require base_path('routes/console.php');
-    }
+		require base_path('routes/console.php');
+	}
 }
---
+++ app/Console/Traits/DatabaseEditor.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Console\Traits;
+
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+trait DatabaseEditor
+{
+	/**
+	 * Request local database details from user.
+	 *
+	 * @return array
+	 */
+	protected function requestDatabaseCredentials()
+	{
+		return [
+			'DB_HOST' => $this->ask('Database host', '127.0.0.1'),
+			'DB_DATABASE' => $this->ask('Database name', 'sportr'),
+			'DB_PORT' => $this->ask('Database port', 3306),
+			'DB_USERNAME' => $this->ask('Database user', 'root'),
+			'DB_PASSWORD' => $this->ask('Database password (leave blank for no password)'),
+		];
+	}
+
+	/**
+	 * Creates all necessary database tables.
+	 *
+	 * @param  array $updatedCreds
+	 * @param  bool $docker
+	 * @return void
+	 */
+	protected function migrateDatabase(array $updatedCreds = null, bool $docker = false)
+	{
+		if ($updatedCreds) {
+			$this->updateMysqlConfig($updatedCreds);
+		}
+
+		return $docker ? exec('vendor/bin/sail artisan migrate') : $this->call('migrate');
+	}
+
+	protected function updateMysqlConfig(array $updatedConfig)
+	{
+		foreach ($updatedConfig as $key => $value) {
+			$configKey = strtolower(str_replace('DB_', '', $key));
+
+			if ($configKey === 'password' && $value == 'null') {
+				config(["database.connections.mysql.{$configKey}" => '']);
+
+				continue;
+			}
+
+			config(["database.connections.mysql.{$configKey}" => $value]);
+		}
+	}
+
+	protected function seedDatabase($docker = false)
+	{
+		if ($docker) {
+			exec('vendor/bin/sail artisan db:seed');
+		} else {
+			$this->call('db:seed');
+		}
+	}
+
+	/**
+	 * Checks if database is migrated.
+	 *
+	 * @return bool
+	 */
+	protected function databaseIsMigrated()
+	{
+		return DB::table('migrations')->where('migration', '2020_01_15_193742_create_roles_table')->exists();
+	}
+
+	/**
+	 * Drops all existing database tables.
+	 *
+	 * @return void
+	 */
+	protected function resetDatabase()
+	{
+		$this->call('migrate:reset');
+		Schema::dropIfExists('migrations');
+	}
+
+	/**
+	 * Converts QueryException to clean & human-readable
+	 * error log message in console.
+	 *
+	 * @param  \Illuminate\Database\QueryException
+	 * @return void
+	 */
+	protected function printQueryException(QueryException $e)
+	{
+		switch ($e->errorInfo[1]) {
+		   case 2002: return $this->error('<mysql> Could not find the specified database.');
+		   default: return $this->error('<mysql> An undefined database error occured.');
+	   }
+	}
+}
