205f030035f0cf4efae13ac209c2420d4455b008
Felix Grohs <me@felixgrohs.at>
2021-03-08 18:11:38 +0100

    refactor setup:local command

--- app/Console/Commands/SetupLocalCommand.php
+++ app/Console/Commands/SetupLocalCommand.php
@@ -9,71 +9,71 @@ use Illuminate\Database\QueryException;
 
 class SetupLocalCommand extends Command
 {
-    use EnvEditor;
-    use DatabaseEditor;
+	use EnvEditor;
+	use DatabaseEditor;
 
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'setup:local';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'setup:local';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Prepares Sportr to run properly on your system.';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Prepares Sportr to run properly on your system.';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
-    public function handle()
-    {
-        $this->info('>> Setting up Sportr locally..');
-        $this->setupEnv();
+	/**
+	 * Execute the console command.
+	 *
+	 * @return int
+	 */
+	public function handle()
+	{
+		$this->info('>> Setting up Sportr locally..');
+		$this->setupEnv();
 
-        if ($this->confirm('>> Would you like to setup the .env interactively?', true)) {
-            $this->updateEnv($updatedEnv = $this->requestDatabaseCredentials());
-        } else {
-            $this->warn('<warn> Make sure your .env file is configured correctly before proceeding.');
-            if (! $this->confirm('>> Ready to migrate the specified database ?', true)) {
-                $this->error('<abort> Rerun `php artisan setup:local` when your database is prepared.');
+		if ($this->confirm('>> Would you like to setup a mysql database connection interactively?', true)) {
+			$this->updateEnv($updatedData = $this->requestDatabaseCredentials());
+		} else {
+			$this->warn('<warn> Make sure the .env file is configured correctly before proceeding.');
+			if (!$this->confirm('>> Ready to migrate the specified database ?', true)) {
+				$this->error('<abort> Rerun `php artisan setup:local` when your database is prepared.');
 
-                return 1;
-            }
-        }
+				return 1;
+			}
+		}
 
-        $this->call('db:make');
+		exec('php artisan db:make');
 
-        try {
-            $this->migrateDatabase($updatedEnv ?? null);
-            $this->seedDatabase();
-        } catch (QueryException $e) {
-            $this->printQueryException($e);
+		try {
+			$this->migrateDatabase($updatedData ?? null);
+			$this->seedDatabase();
+		} catch (QueryException $e) {
+			$this->printQueryException($e);
 
-            return 1;
-        }
+			return 1;
+		}
 
-        $this->callSilently('cache:clear');
-        $this->callSilently('storage:link');
+		$this->callSilently('cache:clear');
+		$this->callSilently('storage:link');
 
-        $this->info('>> Sportr is ready! Have fun :)');
-        $this->comment('>> Run `php artisan serve` to start a development server.');
+		$this->info('>> Sportr is ready! Have fun :)');
+		$this->comment('>> Run `php artisan serve` to start a development server.');
 
-        return 0;
-    }
+		return 0;
+	}
 }
